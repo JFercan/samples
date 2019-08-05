cf547762231b270f74b558a871a1e4e26a595458
Fernando Grijalba <jfercan@hotmail.com>
2019-08-05 08:58:46 -0400

    Add project files.

---
+++ WpfApp1/DelegatedCommand.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action _executeMethod;
+        private readonly Func<bool> _canExecuteMethod;
+
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute()
+        {
+            if (_canExecuteMethod != null)
+            {
+                return _canExecuteMethod();
+            }
+            return true;
+        }
+
+        public void Execute()
+        {
+            if (_executeMethod != null)
+            {
+                _executeMethod();
+            }
+        }
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute();
+        }
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private readonly Action<T> _executeMethod;
+        private readonly Func<T, bool> _canExecuteMethod;
+
+        #region Constructors
+        public DelegateCommand(Action<T> executeMethod)
+        {
+            _executeMethod = executeMethod;
+        }
+        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+        #endregion
+
+        #region Public Methods
+        public bool CanExecute(T parameter)
+        {
+            if (_canExecuteMethod != null)
+            {
+                return _canExecuteMethod(parameter);
+            }
+            return true;
+        }
+
+        public void Execute(T parameter)
+        {
+            if (_executeMethod != null)
+            {
+                _executeMethod(parameter);
+            }
+        }
+        #endregion
+
+        #region ICommand Members
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (parameter == null &&
+                typeof(T).IsValueType)
+            {
+                return _canExecuteMethod == null;
+            }
+            return CanExecute((T)parameter);
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute((T)parameter);
+        }
+
+        #endregion
+    }
+}
---
+++ WpfApp1/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private bool _isEnglish = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string LanguageCommandLabel { get { return Properties.Resources.LanguageCommandLabel; } }
+        public string NameLable { get { return Properties.Resources.NameLable; } }
+
+        private string _name;
+        public string Name {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged("Name"); }
+        }
+
+        private ICommand _command;
+        public ICommand LanguageCommand {
+            get { return _command ?? (_command = new DelegateCommand(ExecuteCommand, () => { return true; })); }
+        }
+
+        private void ExecuteCommand()
+        {
+            if (_isEnglish)
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("es");
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("es");
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
+            }
+            RefreshLables();
+            _isEnglish = !_isEnglish;
+        }
+
+        private void RefreshLables()
+        {
+            OnPropertyChanged(nameof(LanguageCommandLabel));
+            OnPropertyChanged(nameof(NameLable));
+        }
+    }
+}
